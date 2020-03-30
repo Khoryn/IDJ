d074e0074cec09eac17a6497489afbf9936028b0
Bernardo <bmckay1996@hotmail.com>
2020-03-30 16:42:44 +0100

    Added Unique Students Exercise

---
+++ UniqueStudentNames/UniqueStudentNames/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueStudentNames
+{
+    class Student
+    {
+        public string name;
+
+        public Student(string n)
+        {
+            name = n;
+        }
+    }
+}
---
+++ UniqueStudentNames/UniqueStudentNames/StudentGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueStudentNames
+{
+    class StudentGroup
+    {
+        public static HashSet<string> students = new HashSet<string>();
+
+
+        public static void ReceiveNames()
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                Student newStudent = new Student(command);
+                students.Add(newStudent.name);
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Unique student names :" + students.Count);
+        }
+    }
+}
