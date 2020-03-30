b95c0d2212fc6e06d3855551746b50945b50dd09
Bernardo <bmckay1996@hotmail.com>
2020-03-30 16:54:14 +0100

    Added more

---
+++ TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] cmdWords = command.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                switch (cmdWords[1])
+                {
+                    case "Celsius": Console.WriteLine(Temperature.ConvertToFahrenheit(int.Parse(cmdWords[0]))); break;
+                    case "Fahrenheit": Console.WriteLine(Temperature.ConvertToCelsius(int.Parse(cmdWords[0]))); break;
+                    default: Console.WriteLine("Não encontrou a unidade"); break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConverter/Temperature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    class Temperature
+    {
+        public static string ConvertToFahrenheit(int degreesCelsius)
+        {
+            double degreesInFahrenheit = degreesCelsius * 1.8f + 32;
+            return String.Format("{0:F2}",degreesInFahrenheit);
+        }
+
+        public static string ConvertToCelsius(int degreesInFahrenheit)
+        {
+            double degreesCelsius = (degreesInFahrenheit - 32) / 1.8f;
+            return String.Format("{0:F2}", degreesCelsius);
+        }
+    }
+}
